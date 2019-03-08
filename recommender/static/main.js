5b1ac4e293034f26b4d7348e46cd7e4861509e90
Chung Yin Cheung <cxc574@cs.bham.ac.uk>
2019-03-08 02:52:33 +0000

    'Delete favorites' implemented

--- recommender/static/main.js
+++ recommender/static/main.js
@@ -13,27 +13,32 @@ function search() {
             var responseText = request.responseText;
             var search_results = JSON.parse(responseText).results;
 
-            // For each searched title render a <button> element
+            // For each searched title render a button within the search_result <div>
             for (i = 0; i < search_results.length; i++) {
                 var title = search_results[i].split('::')[0];
                 var type = search_results[i].split('::')[1];
                 var button = document.createElement('button');
                 button.classList.add('list-group-item');
                 button.classList.add('list-group-item-action');
-                button.value = search_results[i];
-                button.textContent = title + ' (' + type + ')';
+                button.id = search_results[i];
+                button.textContent = title + ' (' + type + ')';  
+                document.getElementById('search_results').appendChild(button);
+
+                // On mouse down listener for the button
                 button.onmousedown = function() {
                     clearSearchResults();
+
+                    // Clear the search bar
                     document.getElementById('search').value = '';
 
-                    if (!arrayContains(favorites, this.value)) {
-                        favorites.push(this.value);
+                    // Update the favorites array and list display
+                    if (!arrayContains(favorites, this.id)) {
+                        favorites.push(this.id);
                         console.log(favorites);
                         update_favorites_display();
                     }
 
                 };
-                document.getElementById('search_results').appendChild(button);
             }
         }
 
@@ -43,25 +48,9 @@ function search() {
 
 // Remove all buttons within search_results <div>
 function clearSearchResults() {
-    var search_results = document.getElementById("search_results");
+    var search_results = document.getElementById('search_results');
     while (search_results.firstChild) {
         search_results.removeChild(search_results.firstChild);
-    }
-}
-
-function update_favorites_display() {
-    resetFavoritesDisplay();
-
-    for (i = 0; i < favorites.length; i++) {
-        var title = favorites[i].split('::')[0];
-        var type = favorites[i].split('::')[1];
-        var button = document.createElement('button');
-        button.classList.add('list-group-item');
-        button.classList.add('list-group-item-action');
-        button.value = title;
-        button.textContent = title + ' (' + type + ')';
-        button.draggable = true;
-        document.getElementById('favorites').appendChild(button);
     }
 }
 
@@ -69,6 +58,38 @@ function resetFavorites() {
     favorites = [];
     console.log(favorites);
     resetFavoritesDisplay();
+}
+
+function update_favorites_display() {
+    resetFavoritesDisplay();
+
+    for (i = 0; i < favorites.length; i++) {
+        // Create a list item for the favorite item
+        var title = favorites[i].split('::')[0];
+        var type = favorites[i].split('::')[1];
+        var li = document.createElement('li');
+        li.classList.add('list-group-item');
+        li.id = favorites[i];
+        li.textContent = title + ' (' + type + ')';
+        document.getElementById('favorites').appendChild(li);
+
+        // Create a delete button for the favorite item
+        var delete_button = document.createElement('button');
+        delete_button.id = favorites[i];
+        delete_button.type = 'button';
+        delete_button.classList.add('close');
+        delete_button.classList.add('float-right');
+        delete_button.innerHTML = '&times;';
+        li.appendChild(delete_button);
+
+        // On mouse down listener for the delete button
+        delete_button.onmousedown = function() {
+            var favoriteToBeRemovedIndex = favorites.indexOf(this.id);
+            favorites.splice(favoriteToBeRemovedIndex, 1);
+            var liToBeRemoved = document.getElementById(this.id);
+            liToBeRemoved.parentNode.removeChild(liToBeRemoved);
+        };
+    }
 }
 
 // Remove all buttons within favorites <div>
